f8ed63b1c8dc293b762013d7e98204d20e608b61
Ihor Polyakov <ihor.polyakov@gmail.com>
2016-06-30 08:51:49 +0300

    Calculator improvements & new results & diagrams
    * calculator trying to find opponent not only randomly, but filtering by rating otherwise
    * zero rating users stop being processed
    * max_rating calculation fix

--- rating_calculation/calculator.py
+++ rating_calculation/calculator.py
@@ -17,7 +17,7 @@ class Calculator:
     def __init__(self):
         pass
 
-    def read(self, file, start_rating, limit):
+    def read(self, file, rating, limit):
         i = 0
         for line in file:
             line = re.split('[^\d]', str(line).strip(), 2)
@@ -27,7 +27,7 @@ class Calculator:
             uid, might = map(int, line)
             self.users[uid] = {
                 'id': uid,
-                'rating': start_rating,
+                'rating': rating,
                 'might': might,
                 'fights': 0,
             }
@@ -48,6 +48,8 @@ class Calculator:
                 if process_finish - process_start > 5:
                     eprint('%.2f%% - duration: %.5f sec' % (100 * i / total, process_finish - process_start))
                     process_start = time()
+                if user['rating'] < 0:
+                    continue
                 opponent = self.find_opponent(user)
                 if not opponent:
                     continue
@@ -58,17 +60,25 @@ class Calculator:
                 user['fights'] += 1
 
     def find_opponent(self, user):
-        for _ in range(100):
+        for _ in range(max(100, math.ceil(self.length / 100))):
             user_id = self.user_ids[random.randint(0, self.length - 1)]
             opponent = self.users[user_id]
             if self.check_restriction(user, opponent):
                 return opponent
+        min_rating, max_rating = self.get_rating_limits(user)
+        fits = [user_id for user_id, op in self.users.items() if
+                user_id != user['id'] and min_rating <= op['rating'] <= max_rating]
+        length = len(fits)
+        if length > 0:
+            user_id = fits[random.randint(0, length - 1)]
+            return self.users[user_id]
         return False
 
     def print(self):
         print('id,rating,might,fights')
         for _, user in self.users.items():
             print(','.join(map(str, [user['id'], user['rating'], user['might'], user['fights']])))
+        eprint('Fights: %d' % sum(u['fights'] for _, u in self.users.items()))
 
     @staticmethod
     def battle_formula(attacker_might, defender_might):
@@ -84,7 +94,13 @@ class Calculator:
     def check_restriction(user, opponent):
         if user['id'] == opponent['id']:
             return False
-        min_rating = max(0, user['rating'] - user['rating'] * Calculator.rating_diff_percent / 100)
-        max_rating = user['rating'] + user['rating'] * Calculator.rating_diff_percent / 100
+        min_rating, max_rating = Calculator.get_rating_limits(user)
 
         return min_rating <= opponent['rating'] <= max_rating
+
+    @staticmethod
+    def get_rating_limits(user):
+        min_rating = max(0, user['rating'] - user['rating'] * Calculator.rating_diff_percent / 100)
+        max_rating = user['rating'] + abs(user['rating']) * Calculator.rating_diff_percent / 100
+
+        return min_rating, max_rating
--- rating_calculation/main.py
+++ rating_calculation/main.py
@@ -6,30 +6,30 @@ from time import time
 from tools import eprint
 from calculator import Calculator
 
-start_rating = 500
+default_rating = 500
 
 parser = argparse.ArgumentParser(description='Calculate users\' rating after fights.')
-parser.add_argument("-r", "--rating", type=int, dest="start_rating", default=start_rating,
-                    help="Starting rating for users (default: %(default)s).")
-parser.add_argument("-l", "--limit", type=int, dest="limit", default=10,
+parser.add_argument("--default-rating", type=int, dest="default_rating", default=default_rating,
+                    help="Default rating for users (default: %(default)s).")
+parser.add_argument("--limit", type=int, dest="limit", default=10,
                     help="Number of users to read from file (default: %(default)s).")
-parser.add_argument("-f", "--fights", type=int, dest="fights", default=1,
+parser.add_argument("--fights", type=int, dest="fights", default=1,
                     help="Number of fights to calculate (default: %(default)s).")
 parser.add_argument("--file", type=argparse.FileType('r'), dest="file", default='users.txt',
                     help="Source file with data line-by-line: 'uid\\tmight' (default: %(default)s).")
 
 args = parser.parse_args()
-start_rating = args.start_rating > 0 and args.start_rating or 500
+default_rating = args.default_rating > 0 and args.default_rating or 500
 limit = args.limit > 0 and args.limit or 10000000
 fights = args.fights > 0 and args.fights or 1
 file = args.file
 eprint(args)
 
 calc = Calculator()
-calc.read(file, start_rating, limit)
+calc.read(file, default_rating, limit)
 start = time()
 calc.process(fights)
 finish = time()
 calc.print()
 
-eprint('%.5f sec' % (finish - start))
+eprint('Duration: %.5f sec' % (finish - start))
